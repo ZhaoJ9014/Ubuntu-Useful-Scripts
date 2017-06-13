4ee30e6d75d2ae4beab08627fa41da8ccd02ebbf
Zhao Jian <zhaojian90@u.nus.edu>
2017-06-13 15:04:55 +0700

    Create Search_and_Mount_Disk.sh

--- Search_and_Mount_Disk.sh
+++ Search_and_Mount_Disk.sh
@@ -1,5 +1,7 @@
 sudo lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT,LABEL
 
-sudo mkdir /media/usb
+sudo mkdir /media/usb_1
+sudo mkdir /media/usb_2
 
-sudo mount -t exfat /dev/sdd1 /media/usb
+sudo mount -t exfat /dev/sdd1 /media/usb_1
+sudo mount -t ntfs /dev/sde1 /media/usb_2
