1ad1d50646425f43a3af028f0ec292d90e87f307
Zhao Jian <zhaojian90@u.nus.edu>
2018-06-25 19:43:54 +0800

    Update Mount_Remote.sh

--- Mount_Remote.sh
+++ Mount_Remote.sh
@@ -1,2 +1,2 @@
 sshfs -o idmap=user your_username@your_remote_ip_address:your_remote_document_dir your_local_document_dir
-# e.g. sshfs -o idmap=user zhaojian@172.16.127.42:/home/zhaojian/ /Users/zhaojian/Desktop/DEEP
+# e.g. sshfs -o idmap=user zhaojian@172.16.126.205:/home/zhaojian/ /Users/zhaojian/Desktop/DEEP
